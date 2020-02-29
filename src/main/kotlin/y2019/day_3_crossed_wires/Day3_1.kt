efa301557897c20534805835ec253e63e29436e3
TheBestPessimist <cristian@tbp.land>
2020-02-29 23:33:23 +0200

    Add Day 3, problem 1

---
+++ src/main/kotlin/y2019/day_3_crossed_wires/Day3_1.kt
@@ -0,0 +1,103 @@
+package y2019.day_3_crossed_wires
+
+import loadResourceFile
+import kotlin.math.abs
+
+fun main() {
+    val paths = loadResourceFile("./y2019/day_3_crossed_wires/in1.txt").split("""(\s+)""".toRegex())
+
+    val wire1PathStr = paths[0]
+    val wire2PathStr = paths[1]
+
+    println(solve(wire1PathStr, wire2PathStr))
+}
+
+fun solve(wire1PathStr: String, wire2PathStr: String): Int {
+    val wire1 = createWire(wire1PathStr.split(","))
+    val wire2 = createWire(wire2PathStr.split(","))
+
+    var intersection = wire1.positions.intersect(wire2.positions)
+
+    val origin0x0 = Position(0, 0)
+
+    intersection = intersection.minus(origin0x0)
+    val minPosition = intersection.minBy { manhattanDistance(origin0x0, it) }!!
+
+    return manhattanDistance(origin0x0, minPosition)
+}
+
+fun manhattanDistance(p1: Position, p2: Position): Int {
+    return abs(p1.x - p2.x) + abs(p1.y - p2.y)
+}
+
+fun createWire(wirePath: List<String>): Wire {
+    val wirePositions = mutableSetOf<Position>()
+    var lastPosition = Position(0, 0)
+    for (w in wirePath) {
+        val wireAndLastPos = moveWireOnSingleDirection(w, lastPosition)
+        lastPosition = wireAndLastPos.lastPosition
+        wirePositions.addAll(wireAndLastPos.wire.positions)
+    }
+    return Wire(wirePositions.toSet())
+}
+
+fun moveWireOnSingleDirection(dirAndSteps: String, originPosition: Position): WireAndLastPos {
+    val direction = Direction.of(dirAndSteps[0].toString())
+    val steps = dirAndSteps.substring(1).toInt()
+
+    val wirePositions = mutableSetOf<Position>()
+    var lastPosition = originPosition
+
+    when (direction) {
+        Direction.RIGHT -> {
+            for (x in originPosition.x..originPosition.x + steps) {
+                val pos = Position(x, originPosition.y)
+                wirePositions.add(pos)
+                lastPosition = pos
+            }
+        }
+        Direction.LEFT -> {
+            for (x in originPosition.x downTo originPosition.x - steps) {
+                val pos = Position(x, originPosition.y)
+                wirePositions.add(pos)
+                lastPosition = pos
+            }
+        }
+        Direction.UP -> {
+            for (y in originPosition.y..originPosition.y + steps) {
+                val pos = Position(originPosition.x, y)
+                wirePositions.add(pos)
+                lastPosition = pos
+            }
+        }
+        Direction.DOWN -> {
+            for (y in originPosition.y downTo originPosition.y - steps) {
+                val pos = Position(originPosition.x, y)
+                wirePositions.add(pos)
+                lastPosition = pos
+            }
+
+        }
+    }
+    return WireAndLastPos(Wire(wirePositions), lastPosition)
+}
+
+data class WireAndLastPos(val wire: Wire, val lastPosition: Position)
+
+data class Position(val x: Int, val y: Int)
+
+data class Wire(val positions: Set<Position>)
+
+private enum class Direction(val direction: String) {
+    UP("U"),
+    DOWN("D"),
+    LEFT("L"),
+    RIGHT("R");
+
+    companion object {
+        private val map = values().associateBy(Direction::direction)
+        fun of(direction: String): Direction {
+            return map[direction] ?: throw IllegalArgumentException()
+        }
+    }
+}
---
+++ src/test/kotlin/y2019/day_3_crossed_wires/MoveWireOnSingleDirectionTest.kt
@@ -0,0 +1,102 @@
+package y2019.day_3_crossed_wires
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class MoveWireOnSingleDirectionTest {
+
+    @Test
+    fun `R8 From 0x0`() {
+        val directionAndSteps = "R8"
+        val originPosition = Position(0, 0)
+
+        val (wire, lastPosition) = moveWireOnSingleDirection(directionAndSteps, originPosition)
+
+        assertEquals(Position(8, 0), lastPosition)
+
+        val expectedWire = Wire(setOf(
+                Position(0, 0),
+                Position(1, 0),
+                Position(2, 0),
+                Position(3, 0),
+                Position(4, 0),
+                Position(5, 0),
+                Position(6, 0),
+                Position(7, 0),
+                Position(8, 0)
+        ))
+        assertEquals(expectedWire, wire)
+    }
+
+
+    @Test
+    fun `L8 From 0x0`() {
+        val directionAndSteps = "L8"
+        val originPosition = Position(0, 0)
+
+        val (wire, lastPosition) = moveWireOnSingleDirection(directionAndSteps, originPosition)
+
+        assertEquals(Position(-8, 0), lastPosition)
+
+        val expectedWire = Wire(setOf(
+                Position(0, 0),
+                Position(-1, 0),
+                Position(-2, 0),
+                Position(-3, 0),
+                Position(-4, 0),
+                Position(-5, 0),
+                Position(-6, 0),
+                Position(-7, 0),
+                Position(-8, 0)
+        ))
+        assertEquals(expectedWire, wire)
+    }
+
+
+    @Test
+    fun `U8 From 0x0`() {
+        val directionAndSteps = "U8"
+        val originPosition = Position(0, 0)
+
+        val (wire, lastPosition) = moveWireOnSingleDirection(directionAndSteps, originPosition)
+
+        assertEquals(Position(0, 8), lastPosition)
+
+        val expectedWire = Wire(setOf(
+                Position(0, 0),
+                Position(0, 1),
+                Position(0, 2),
+                Position(0, 3),
+                Position(0, 4),
+                Position(0, 5),
+                Position(0, 6),
+                Position(0, 7),
+                Position(0, 8)
+        ))
+        assertEquals(expectedWire, wire)
+    }
+
+
+    @Test
+    fun `D8 From 0x0`() {
+        val directionAndSteps = "D8"
+        val originPosition = Position(0, 0)
+
+        val (wire, lastPosition) = moveWireOnSingleDirection(directionAndSteps, originPosition)
+
+        assertEquals(Position(0, -8), lastPosition)
+
+        val expectedWire = Wire(setOf(
+                Position(0, 0),
+                Position(0, -1),
+                Position(0, -2),
+                Position(0, -3),
+                Position(0, -4),
+                Position(0, -5),
+                Position(0, -6),
+                Position(0, -7),
+                Position(0, -8)
+        ))
+        assertEquals(expectedWire, wire)
+    }
+}
