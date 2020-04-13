5994acf12ac5f0b2be758bf0ff1b48bf9f4cc76f
TheBestPessimist <cristian@tbp.land>
2020-04-13 19:45:09 +0300

    CPU is now designed as an instance with everything contained. Cleanup all the method parameters

--- src/main/kotlin/land/tbp/y2019/intcode/computer/CPU.kt
+++ src/main/kotlin/land/tbp/y2019/intcode/computer/CPU.kt
@@ -2,12 +2,16 @@ package land.tbp.y2019.intcode.computer
 
 import land.tbp.y2019.intcode.computer.instructions.*
 
-class CPU(private val instructions: List<Instruction>, private val memory: Memory, private val inputs: MutableList<Int>, private val outputs: MutableList<Int>) {
+class CPU(
+        private val instructions: List<Instruction>,
+        private val memory: Memory,
+        private val inputs: MutableList<Int>,
+        private val outputs: MutableList<Int>
+) {
 
     private var programCounter = 0
 
     fun runProgram(): Int {
-
         while (true) {
             val opcode = memory.read(programCounter)
             val instruction: Instruction = fetchAndDecode(opcode)
@@ -15,7 +19,7 @@ class CPU(private val instructions: List<Instruction>, private val memory: Memor
             if (HaltInstruction == instruction) {
                 return memory.read(0)
             }
-            execute(instruction, programCounter, memory, inputs, outputs)
+            execute(instruction)
             programCounter += instruction.size
         }
     }
@@ -30,38 +34,36 @@ class CPU(private val instructions: List<Instruction>, private val memory: Memor
         throw Exception("Unknown Instruction $instructionCode")
     }
 
-    private fun execute(instruction: Instruction, instructionPointer: Int, memory: Memory, inputs: MutableList<Int>, outputs: MutableList<Int>) {
-        val instructionCode = memory.read(instructionPointer)
-
+    private fun execute(instruction: Instruction) {
+        val instructionCode = memory.read(programCounter)
 
         // This if will come later to bite my ass.
         // I believe the IO should be added to the normal flow and instructions, but i'm too lazy to do that now.
         if (instruction in listOf(InputInstruction, OutputInstruction)) {
-            handleIOInstructions(instruction, memory, instructionPointer, inputs, outputs)
+            handleIOInstructions(instruction)
         } else {
-            handleNonIOInstructions(instruction, instructionCode, instructionPointer, memory)
+            val parameterModes = instruction.computeParameterModes(instructionCode)
+            handleNonIOInstructions(instruction, parameterModes)
         }
     }
 
-    private fun handleIOInstructions(instruction: Instruction, memory: Memory, instructionPointer: Int, inputs: MutableList<Int>, outputs: MutableList<Int>) {
+    private fun handleIOInstructions(instruction: Instruction) {
         if (instruction == InputInstruction) {
             val outputValue = inputs.removeAt(0)
-            val writeToAddress = memory.read(instructionPointer + instruction.numberOfParameters)
+            val writeToAddress = memory.read(programCounter + instruction.numberOfParameters)
             memory.set(writeToAddress, outputValue)
         } else if (instruction == OutputInstruction) {
-            val readFromAddress = memory.read(instructionPointer + instruction.numberOfParameters)
+            val readFromAddress = memory.read(programCounter + instruction.numberOfParameters)
             val outputValue = memory.read(readFromAddress)
             outputs.add(outputValue)
             //todo maybe i should implement CPU.ReadMemoryByParamMode, so that i don't bother with calculating ReadFrom/WriteTo addresses
         }
     }
 
-    private fun handleNonIOInstructions(instruction: Instruction, opCode: Int, instructionPointer: Int, memory: Memory) {
-        val parameterModes = instruction.computeParameterModes(opCode)
-
+    private fun handleNonIOInstructions(instruction: Instruction, parameterModes: List<InstructionParameterMode>) {
         val inputValues = mutableListOf<Int>()
         for (i in 0 until instruction.numberOfParameters - 1) {
-            val positionOrValue = instructionPointer + 1 + i
+            val positionOrValue = programCounter + 1 + i
             val inputValue = when (parameterModes[i]) {
                 InstructionParameterMode.Position -> {
                     val readFromAddress = memory.read(positionOrValue)
@@ -73,7 +75,7 @@ class CPU(private val instructions: List<Instruction>, private val memory: Memor
         }
 
         val outputValue: Int = instruction.execute(inputValues)
-        val writeToAddress = memory.read(instructionPointer + instruction.numberOfParameters)
+        val writeToAddress = memory.read(programCounter + instruction.numberOfParameters)
         memory.set(writeToAddress, outputValue)
     }
 }
