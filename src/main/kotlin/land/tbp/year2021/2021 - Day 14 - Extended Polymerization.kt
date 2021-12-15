a7a1dcbcf6d6ba9775803e5db93ddc8ffc516584
TheBestPessimist <cristian@tbp.land>
2021-12-15 10:49:08 +0200

    better names?

--- src/main/kotlin/land/tbp/year2021/2021 - Day 14 - Extended Polymerization.kt
+++ src/main/kotlin/land/tbp/year2021/2021 - Day 14 - Extended Polymerization.kt
@@ -23,27 +23,19 @@ fun `2021 - Day 14 - Extended Polymerization - Part 1`(input: List<String>): Num
 
 fun `2021 - Day 14 - Extended Polymerization - Part 2`(input: List<String>): Number {
     return solve14(input, 40)
-
 }
-
-/*
-B 6
-C 4
-N 2
-H 1
- */
 
 var it = 0L
 
 private fun solve14(input: List<String>, limit: Int): Number {
-    var template = input.first()
+    val template = input.first()
     val map = input.filter { it.contains("->") }
         .associate {
             val s = it.split(" -> ")
             s[0] to s[1]
         }
 
-    val tempGr = template
+    val tempCount = template
         .windowed(2)
         .parallelStream()
         .map { w ->
@@ -53,25 +45,25 @@ private fun solve14(input: List<String>, limit: Int): Number {
         }
 
 
-    val gr = mutableMapOf<String, Long>()
-    tempGr.forEach {
+    val count = mutableMapOf<String, Long>()
+    tempCount.forEach {
         it.forEach { (k, v) ->
-            gr[k] = gr.getOrDefault(k, 0) + v
+            count[k] = count.getOrDefault(k, 0) + v
         }
     }
 
-    gr[template.first().toString()] = gr[template.first().toString()]!! + 1
-    println(gr)
+    count[template.first().toString()] = count[template.first().toString()]!! + 1
+    println(count)
 
-    val min = gr.minOf { it.value }
-    val max = gr.maxOf { it.value }
+    val min = count.minOf { it.value }
+    val max = count.maxOf { it.value }
 
     return max - min
 }
 
-fun step143(c1: String, c2: String, map: Map<String, String>, limit: Int, depth: Int, gr: MutableMap<String, Long>, doCount: Boolean = false) {
+fun step143(parent1: String, parent2: String, map: Map<String, String>, limit: Int, depth: Int, count: MutableMap<String, Long>, doCount: Boolean = false) {
     if (depth == limit && doCount) {
-        inc(c1, c2, gr)
+        incrementCount(parent1, parent2, count)
         return
     }
     if (depth == limit) return
@@ -79,16 +71,16 @@ fun step143(c1: String, c2: String, map: Map<String, String>, limit: Int, depth:
     it++
     if (it % 1_000_000_000 == 0L) {
         println(it)
-        println(gr)
+        println(count)
     }
 
-    val ch = map[c1 + c2]!!
-    step143(c1, ch, map, limit, depth + 1, gr, false)
-    step143(ch, c2, map, limit, depth + 1, gr, true)
+    val middle = map[parent1 + parent2]!!
+    step143(parent1, middle, map, limit, depth + 1, count, false)
+    step143(middle, parent2, map, limit, depth + 1, count, true)
 }
 
 
-fun inc(c1: String, c2: String, gr: MutableMap<String, Long>) {
+fun incrementCount(c1: String, c2: String, gr: MutableMap<String, Long>) {
     gr[c1] = gr.getOrDefault(c1, 0) + 1
     gr[c2] = gr.getOrDefault(c2, 0) + 1
 }
