7fbc1972f47ba43b724a29823c81cfca09c2d919
TheBestPessimist <cristian@tbp.land>
2021-12-19 16:16:03 +0200

    2021 - Day 19 - I have no idea how to do this (2)

--- src/main/kotlin/land/tbp/year2021/2021 - Day 19 - aaaa.kt
+++ src/main/kotlin/land/tbp/year2021/2021 - Day 19 - aaaa.kt
@@ -4,13 +4,13 @@ package land.tbp.year2021
 
 import Point
 import Triangle
-import land.tbp.year2021.`2021 - Day 19 - aaaa`.Companion.adjustPoints
+import containsAny
 import land.tbp.year2021.`2021 - Day 19 - aaaa`.Companion.intersect
 import land.tbp.year2021.`2021 - Day 19 - aaaa`.Companion.makePoint
 import land.tbp.year2021.`2021 - Day 19 - aaaa`.Companion.makeTriangles
+import readInput
 import java.math.BigDecimal
 import java.math.MathContext
-import kotlin.math.abs
 
 
 data class Triangle3d(
@@ -38,94 +38,91 @@ internal class `2021 - Day 19 - aaaa` {
         Repeat this for x y z coordinates
 
          */
-        fun findAdjustment(realt: Triangle, wrongt: Triangle): Point {
-            var adjustment = mutableListOf(0, 0, 0) // the adjustment point will be created at the end
-
-            val realp0 = realt.first.toList()
-            val wrongp0 = wrongt.first.toList()
-            for (coordi in 0..2) {
-                var coordReal = realp0[coordi]
-                var coordWrong = wrongp0[coordi]
-                val delta1 = abs(coordReal) - abs(coordWrong)
-                val delta2 = -abs(coordReal) - abs(coordWrong)
-                val delta3 = -abs(coordReal) - -abs(coordWrong)
-                val delta4 = abs(coordReal) - -abs(coordWrong)
-
-
-
-                if (realt.toList()[0].toList()[coordi] == wrongt.toList()[0].toList()[coordi] + delta1
-                    && realt.toList()[1].toList()[coordi] == wrongt.toList()[1].toList()[coordi] + delta1
-                    && realt.toList()[2].toList()[coordi] == wrongt.toList()[2].toList()[coordi] + delta1
-                ) {
-                    adjustment[coordi] = delta1
-                    continue
-                }
-
-
-                if (realt.toList()[0].toList()[coordi] == wrongt.toList()[0].toList()[coordi] + delta2
-                    && realt.toList()[1].toList()[coordi] == wrongt.toList()[1].toList()[coordi] + delta2
-                    && realt.toList()[2].toList()[coordi] == wrongt.toList()[2].toList()[coordi] + delta2
-                ) {
-                    adjustment[coordi] = delta2
-                    continue
-                }
-
-
-                if (realt.toList()[0].toList()[coordi] == wrongt.toList()[0].toList()[coordi] + delta3
-                    && realt.toList()[1].toList()[coordi] == wrongt.toList()[1].toList()[coordi] + delta3
-                    && realt.toList()[2].toList()[coordi] == wrongt.toList()[2].toList()[coordi] + delta3
-                ) {
-                    adjustment[coordi] = delta3
-                    continue
-
-                }
-
-                if (realt.toList()[0].toList()[coordi] == wrongt.toList()[0].toList()[coordi] + delta4
-                    && realt.toList()[1].toList()[coordi] == wrongt.toList()[1].toList()[coordi] + delta4
-                    && realt.toList()[2].toList()[coordi] == wrongt.toList()[2].toList()[coordi] + delta4
-                ) {
-                    adjustment[coordi] = delta4
-                    continue
-                }
-            }
-            return Point(adjustment[0], adjustment[1], adjustment[2])
-        }
-
-
-        fun adjustPoints(triangles: MutableList<Triangle3d>, matchingTriangles: MutableList<Pair<Triangle3d, Triangle3d>>) {
-            for ((real, wrong) in matchingTriangles) {
-                val realp = real.points.toList()
-                val wrongp = wrong.points.toList()
-
-
-//        for (i in realp.indices) {
-                if (realp[0].first - wrongp[0].first != realp[1].first - wrongp[1].first &&
-                    realp[0].first - wrongp[0].first != realp[2].first - wrongp[2].first
-                )
-                    error("fuck $realp $wrongp")
+//        fun findAdjustment(realt: Triangle, wrongt: Triangle): Point {
+//            var adjustment = mutableListOf(0, 0, 0) // the adjustment point will be created at the end
+//
+//            val realp0 = realt.first.toList()
+//            val wrongp0 = wrongt.first.toList()
+//            for (coordi in 0..2) {
+//                var coordReal = realp0[coordi]
+//                var coordWrong = wrongp0[coordi]
+//                val delta1 = abs(coordReal) - abs(coordWrong)
+//                val delta2 = -abs(coordReal) - abs(coordWrong)
+//                val delta3 = -abs(coordReal) - -abs(coordWrong)
+//                val delta4 = abs(coordReal) - -abs(coordWrong)
+//
+//
+//
+//                if (realt.toList()[0].toList()[coordi] == wrongt.toList()[0].toList()[coordi] + delta1
+//                    && realt.toList()[1].toList()[coordi] == wrongt.toList()[1].toList()[coordi] + delta1
+//                    && realt.toList()[2].toList()[coordi] == wrongt.toList()[2].toList()[coordi] + delta1
+//                ) {
+//                    adjustment[coordi] = delta1
+//                    continue
+//                }
+//
+//
+//                if (realt.toList()[0].toList()[coordi] == wrongt.toList()[0].toList()[coordi] + delta2
+//                    && realt.toList()[1].toList()[coordi] == wrongt.toList()[1].toList()[coordi] + delta2
+//                    && realt.toList()[2].toList()[coordi] == wrongt.toList()[2].toList()[coordi] + delta2
+//                ) {
+//                    adjustment[coordi] = delta2
+//                    continue
+//                }
+//
+//
+//                if (realt.toList()[0].toList()[coordi] == wrongt.toList()[0].toList()[coordi] + delta3
+//                    && realt.toList()[1].toList()[coordi] == wrongt.toList()[1].toList()[coordi] + delta3
+//                    && realt.toList()[2].toList()[coordi] == wrongt.toList()[2].toList()[coordi] + delta3
+//                ) {
+//                    adjustment[coordi] = delta3
+//                    continue
+//
+//                }
+//
+//                if (realt.toList()[0].toList()[coordi] == wrongt.toList()[0].toList()[coordi] + delta4
+//                    && realt.toList()[1].toList()[coordi] == wrongt.toList()[1].toList()[coordi] + delta4
+//                    && realt.toList()[2].toList()[coordi] == wrongt.toList()[2].toList()[coordi] + delta4
+//                ) {
+//                    adjustment[coordi] = delta4
+//                    continue
+//                }
+//            }
+//            return Point(adjustment[0], adjustment[1], adjustment[2])
 //        }
-                val first = real.points.first
-                val second = wrong.points.first
 
 
-                wrong.points
-            }
-            TODO("Not yet implemented")
-        }
+//        fun adjustPoints(triangles: MutableList<Triangle3d>, matchingTriangles: MutableList<Pair<Triangle3d, Triangle3d>>) {
+//            for ((real, wrong) in matchingTriangles) {
+//                val realp = real.points.toList()
+//                val wrongp = wrong.points.toList()
+//
+//
+////        for (i in realp.indices) {
+//                if (realp[0].first - wrongp[0].first != realp[1].first - wrongp[1].first &&
+//                    realp[0].first - wrongp[0].first != realp[2].first - wrongp[2].first
+//                )
+//                    error("fuck $realp $wrongp")
+////        }
+//                val first = real.points.first
+//                val second = wrong.points.first
+//
+//
+//                wrong.points
+//            }
+//            TODO("Not yet implemented")
+//        }
 
-        fun intersect(realPositions: MutableList<Triangle3d>, triangles: MutableList<Triangle3d>): MutableList<Pair<Triangle3d, Triangle3d>>? {
-            val matchingPoints: MutableList<Pair<Triangle3d, Triangle3d>> = mutableListOf()
-            for (i in realPositions.indices) {
-                val currentTriangle = realPositions[i]
+        fun intersect(uniqueBeacons: List<Triangle3d>, triangles: MutableList<Triangle3d>): MutableList<Pair<Triangle3d, List<Triangle3d>>> {
+            val matchingPoints: MutableList<Pair<Triangle3d, List<Triangle3d>>> = mutableListOf()
+
+            for (i in uniqueBeacons.indices) {
+                val currentTriangle = uniqueBeacons[i]
                 val haveSameArea = triangles.filter { it.area == currentTriangle.area && it.pointDistances == currentTriangle.pointDistances }
 
-                if (haveSameArea.isNotEmpty()) matchingPoints += currentTriangle to haveSameArea.single()
-
-                if (matchingPoints.size >= 12) {
-                    return matchingPoints
-                }
+                if (haveSameArea.isNotEmpty()) matchingPoints += currentTriangle to haveSameArea
             }
-            return null
+            return matchingPoints
         }
 
 
@@ -137,7 +134,7 @@ internal class `2021 - Day 19 - aaaa` {
                     for (j in s - i)
                         for (k in s - i - j)
                             if (i != j && j != k && i != k && currScannerTriangles.none { it.points in listOf(Triangle(i, j, k), Triangle(i, k, j), Triangle(j, i, k), Triangle(j, k, i), Triangle(k, i, j), Triangle(k, j, i)) })
-                                currScannerTriangles += Triangle3d(Triangle(i, j, k), areaTriangle3d(i, j, k), distance3d(i, j, k));
+                                currScannerTriangles += Triangle3d(Triangle(i, j, k), areaTriangle3d(i, j, k), distance3d(i, j, k))
                 ret += currScannerTriangles
             }
             return ret
@@ -229,58 +226,110 @@ internal class `2021 - Day 19 - aaaa` {
 }
 
 fun main() {
-    println("not done. 3d space is too difficult for me.")
+//    println("not done. 3d space is too difficult for me.")
 
-//    val inputTest = readInput("land/tbp/year2021/Day19-t")
-//    val input = readInput("land/tbp/year2021/Day19")
+    val inputTest = readInput("land/tbp/year2021/Day19-t")
+    val input = readInput("land/tbp/year2021/Day19")
 
-//    println(`2021 - Day 19 - aaaa - Part 1`(listOf(
-//        "--- scanner 0 ---",
-//        "0,2,0",
-//        "4,1,0",
-//        "3,3,0",
-//        "",
-//        "--- scanner 1 ---",
-//        "-1,-1,0",
-//        "-5,0,0",
-//        "-2,1,0",
-//    )))
+//    println(`2021 - Day 19 - aaaa - Part 2`(listOf("--- scanner 0 ---", "0,2,0", "4,1,0", "3,3,0", "", "--- scanner 1 ---", "-1,-1,0", "-5,0,0", "-2,1,0")))
 
 
 //    println(`2021 - Day 19 - aaaa - Part 1`(inputTest))
 //    println(`2021 - Day 19 - aaaa - Part 1`(input))
 
-//    println(`2021 - Day 19 - aaaa - Part 2`(inputTest))
+    println(`2021 - Day 19 - aaaa - Part 2`(inputTest))
 //    println(`2021 - Day 19 - aaaa - Part 2`(input))
 }
 
 
 fun `2021 - Day 19 - aaaa - Part 1`(input: List<String>): Long {
-    val scanners: MutableList<MutableList<Point>> = makePoint(input.toMutableList())
+//    val scanners: MutableList<MutableList<Point>> = makePoint(input.toMutableList())
+//
+//    val triangles: MutableList<MutableList<Triangle3d>> = makeTriangles(scanners)
+//
+//    val uniqueBeacons: MutableSet<Point> = triangles[0].flatMap { it.points.toList() }.toMutableSet()
+//
+//    repeat(3) {
+//        for (i in triangles.indices) {
+//            for (j in triangles.indices) {
+//                if (i == j) continue
+//
+//                val t1 = triangles[i]
+//                val t2 = triangles[j]
+//                val matchingTriangles = intersect(t1, t2)
+//
+//                while (matchingTriangles.isNotEmpty()) {
+//                    val el = matchingTriangles.removeFirst()
+//                    uniqueBeacons += el.second.points.toList()
+//                }
+//            }
+//        }
+//    }
 
-    val triangles: MutableList<MutableList<Triangle3d>> = makeTriangles(scanners)
-
-    val realPositions = triangles.removeFirst()
-
-    var i = 0
-    while (triangles.isNotEmpty()) {
-        i %= triangles.size
-        val t = triangles[i]
-        val matchingTriangles = intersect(realPositions, t)
-        if (matchingTriangles == null) {
-            i++
-            continue
-        }
-        triangles.removeAt(i)
-//        realPositions +=
-        adjustPoints(t, matchingTriangles)
-    }
+//    var iterator = 0
+//    while (triangles.isNotEmpty()) {
+//        val i = iterator % triangles.size
+//        val t = triangles[i]
+//        val matchingTriangles = intersect(uniqueBeacons.toList() as MutableList<Triangle3d>, t)
+//
+//        while (matchingTriangles.isNotEmpty()) {
+//            val el = matchingTriangles.removeFirst()
+//            t.remove(el.second)
+//        }
+//        if (t.isEmpty()) triangles.removeAt(i)
+//
+//        if (iterator > 10_000) break
+//        iterator++
+//    }
 
 
     return 0
 }
 
 fun `2021 - Day 19 - aaaa - Part 2`(input: List<String>): Long {
+    val scanners: MutableList<MutableList<Point>> = makePoint(input.toMutableList())
+    val trianglesInScanners: MutableList<MutableList<Triangle3d>> = makeTriangles(scanners)
+
+    val allp = scanners.flatten().toMutableSet()
+
+    val uniqueBeacons: MutableSet<Point> = mutableSetOf()
+    val uniqueTriangles: MutableSet<Triangle3d> = mutableSetOf()
+
+    for (i in trianglesInScanners.indices) {
+        for (j in i + 1..trianglesInScanners.lastIndex) {
+            val matchingTriangles = intersect(trianglesInScanners[i], trianglesInScanners[j])
+            while (matchingTriangles.isNotEmpty()) {
+                val el = matchingTriangles.removeFirst()
+                uniqueBeacons += el.first.points.toList()
+                uniqueTriangles += el.first
+
+                var validPoints = trianglesInScanners[i].flatMap { it.points.toList() }.filterNot { it in el.first.points.toList() || it in el.second.flatMap { it.points.toList() } }
+                var t: MutableList<Triangle3d> = mutableListOf()
+                for (ii in trianglesInScanners[i]) {
+                    if (validPoints.containsAll(ii.points.toList())) t += ii
+                }
+                trianglesInScanners[i] = t
+
+                validPoints = trianglesInScanners[j].flatMap { it.points.toList() }.filterNot { it in el.first.points.toList() || it in el.second.flatMap { it.points.toList() } }
+                t = mutableListOf()
+                for (ii in trianglesInScanners[j]) {
+                    if (validPoints.containsAll(ii.points.toList())) t += ii
+                }
+                trianglesInScanners[j] = t
+
+                allp.removeAll(el.first.points.toList().toSet())
+                allp.removeAll(el.second.flatMap { it.points.toList() }.toSet())
+            }
+        }
+    }
+
+    val notUniqueBeacons = trianglesInScanners.flatten().flatMap { it.points.toList() }.toSet()
+
+    allp -= uniqueBeacons - uniqueTriangles.flatMap { it.points.toList() }.toSet()
+
+    println(uniqueBeacons.size)
+    println(notUniqueBeacons.size)
+    println(allp.size)
 
 
     return 0
