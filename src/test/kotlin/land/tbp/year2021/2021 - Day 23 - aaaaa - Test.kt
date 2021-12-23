446230187fc759f459e2afbbd394f5eddb328b6a
TheBestPessimist <cristian@tbp.land>
2021-12-23 19:25:42 +0200

    2021 - Day 23 - aaaaa

---
+++ src/test/kotlin/land/tbp/year2021/2021 - Day 23 - aaaaa - Test.kt
@@ -0,0 +1,19 @@
+package land.tbp.year2021
+
+import land.tbp.year2021.Inclusivity.Exclusive
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import readInput
+
+internal class `2021 - Day 23 - aaaaa - Test` {
+    @Test
+    fun test() {
+
+
+
+
+
+
+    }
+
+}
